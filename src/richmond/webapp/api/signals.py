5a784a190080d8b160894a1070e33320a50c00d3
Simon de Haan <simon@soocial.com>
2010-05-13 07:04:43 +0200

    having the signals accept a PK instead of an instance, easier for serialization across the queue

--- src/richmond/webapp/api/signals.py
+++ src/richmond/webapp/api/signals.py
@@ -1,24 +1,27 @@
 import logging
 from decorator import decorator
+from richmond.webapp.api.models import SentSMS, ReceivedSMS
 
 @decorator
-def asynchronous(f, *args, **kwargs):
+def asynchronous_signal(f, instance):
     logging.debug("I should be calling '%s' asynchronously" % f.__name__)
-    return f(*args, **kwargs)
+    return f(instance.pk)
 
 def post_save_sent_sms_handler(**kwargs):
     post_save_sent_sms(kwargs['instance'])
 
-@asynchronous
-def post_save_sent_sms(sent_sms):
+@asynchronous_signal
+def post_save_sent_sms(sent_sms_id):
+    sent_sms = SentSMS.objects.get(pk=sent_sms_id)
     logging.debug("I should take care of actually sending %s" % sent_sms)
 
 
 def post_save_received_sms_handler(**kwargs):
     post_save_received_sms(kwargs['instance'])
 
-@asynchronous
-def post_save_received_sms(received_sms):
+@asynchronous_signal
+def post_save_received_sms(received_sms_id):
+    received_sms = ReceivedSMS.objects.get(pk=received_sms_id)
     logging.debug("I should take care of doing callbacks for %s" % received_sms)
 
 
