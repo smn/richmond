498ab390d91f71265dde2890c1139f099dba0f76
Simon de Haan <simon@soocial.com>
2010-05-13 06:56:24 +0200

    adding a decorator to mock offloading to backend workers

--- src/richmond/webapp/api/signals.py
+++ src/richmond/webapp/api/signals.py
@@ -1,14 +1,25 @@
 import logging
+from decorator import decorator
+
+@decorator
+def asynchronous(f, *args, **kwargs):
+    logging.debug("I should be calling '%s' asynchronously" % f.__name__)
+    return f(*args, **kwargs)
 
 def post_save_sent_sms_handler(**kwargs):
     post_save_sent_sms(kwargs['instance'])
 
+@asynchronous
 def post_save_sent_sms(sent_sms):
-    logging.debug("I should push %s to the queue" % sent_sms)
+    logging.debug("I should take care of actually sending %s" % sent_sms)
 
 
 def post_save_received_sms_handler(**kwargs):
     post_save_received_sms(kwargs['instance'])
 
+@asynchronous
 def post_save_received_sms(received_sms):
-    logging.debug('I should push %s to the queue' % received_sms)
+    logging.debug("I should take care of doing callbacks for %s" % received_sms)
+
+
+
