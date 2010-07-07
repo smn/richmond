75a603d313ddb6605adba777f4587c64891661cf
Simon de Haan <simon@soocial.com>
2010-07-07 17:26:13 +0200

    fixing start celery script

--- start-celery.sh
+++ start-celery.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Poor man's daemonizing of celeryd
 source ./ve/bin/activate && \
-    ./manage.py celeryd --settings=$1 --logfile ./logs/celeryd.$1.$2.log && \
+    ./manage.py celeryd \
+        --settings=$1 \
+        --logfile=./logs/celeryd.$1.$2.log \
+        --concurrency=1 && \
     deactivate
