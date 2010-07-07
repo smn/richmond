ffce84987e1dc39ea27ea466b4228c468b81e324
Simon de Haan <simon@soocial.com>
2010-07-07 17:48:27 +0200

    verbose logging

--- start-celery.sh
+++ start-celery.sh
@@ -4,5 +4,6 @@ source ./ve/bin/activate && \
     ./manage.py celeryd \
         --settings=$1 \
         --logfile=./logs/celeryd.$1.$2.log \
+        --loglevel=debug \
         --concurrency=1 && \
     deactivate
