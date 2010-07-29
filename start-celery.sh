f7389cc21a8381c0210793487dc180816ac7effe
Simon de Haan <simon@soocial.com>
2010-07-29 10:55:22 +0200

    removing start-celery.sh, no longer needed now that we have daemonize

--- start-celery.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Poor man's daemonizing of celeryd
-source ./ve/bin/activate && \
-    ./manage.py celeryd \
-        --settings=$1 \
-        --logfile=./logs/celeryd.$1.$2.log \
-        --loglevel=debug \
-        --concurrency=1 && \
-    deactivate
