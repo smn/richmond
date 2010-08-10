927c7301ca10678a4ab4c8afdbde370e38668d33
Simon de Haan <simon@soocial.com>
2010-08-10 13:25:56 +0200

    new menu

--- src/richmond/campaigns/mm_demo.py
+++ src/richmond/campaigns/mm_demo.py
@@ -5,7 +5,8 @@ from twisted.python import log
 from alexandria.client import Client
 from alexandria.sessions.backend import DBBackend
 from alexandria.sessions.manager import SessionManager
-from alexandria.dsl.core import MenuSystem, prompt, end
+from alexandria.sessions.db import models
+from alexandria.dsl.core import MenuSystem, prompt, end, case
 from alexandria.dsl.validators import pick_one
 
 class VumiDBClient(Client):
@@ -24,6 +25,23 @@ class VumiDBClient(Client):
             reply_type = SessionType.existing
         return self.send_callback(self.id, text, reply_type)
 
+def save_to_session(key, value):
+    while True:
+        ms, session = yield
+        session[key] = value
+        yield False, False
+
+def industry_stats():
+    return "These are the industry stats!"
+
+def expectations_stats():
+    return "These are the expectations!"
+
+def returning_user(menu, session):
+    return session.get('completed', False)
+
+def new_user(*args, **kwargs):
+    return not returning_user(*args, **kwargs)
 
 class VumiConsumer(Consumer):
     
@@ -31,23 +49,39 @@ class VumiConsumer(Consumer):
     Describe the menu system we're running
     """
     menu = MenuSystem(
-        prompt('Hello! What is your name?'),
-        prompt('In what industry are you involved?', options=(
-            'Retail',
-            'Marketing',
-            'Financial/Banking',
-            'IT/Technology',
-            'FMCG',
-            'Travel',
-            'Media',
-            'Other',
-        ), validator=pick_one),
-        prompt('How are you finding the conference?', options=(
-            'Meeting my expectations',
-            'Exceeding my expectations',
-            'Not meeting my expectations',
-        ), validator=pick_one),
-        end('Thanks!')
+        case(
+            (new_user, prompt('Welcome to the Praekelt Star menu system. ' +\
+                                    'What is your first name?', save_as='name')),
+            (returning_user, prompt('Welcome back %(name)s'))
+        ),
+        case(
+            (new_user, prompt('What industry are you from?', options=(
+                'Marketing',
+                'Industry',
+                'Retail',
+                'Financial/Banking',
+                'IT/Technology',
+                'Media',
+                'Other'
+            ), save_as='industry', validator=pick_one)),
+            (returning_user, prompt(industry_stats(), options=('Continue',)))
+        ),
+        case(
+            (new_user, prompt('How are you finding the conference?', options=(
+                'Meeting my expectations',
+                'Exceeding my expectations',
+                'Not meeting my expectations',
+            ), save_as='expectations', validator=pick_one)),
+            (returning_user, prompt(expectations_stats(), options=('Continue',)))
+        ),
+        save_to_session('completed', True),
+        # sms(
+        #     'Hi %(name)s want to know more about Vumi and Star menus? ' + \
+        #     'Visit http://www.praekelt.com'
+        # ),
+        end('Thanks for taking part. You can view real-time statistics on ' + \
+            'the Praekelt screens, or by dialing back into the Star menu ' + \
+            ' system!')
     )
     
     def new_ussd_session(self, msisdn, message):
