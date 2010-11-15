483be4d734118df1c44307847b870334e2712837
Simon de Haan <simon@soocial.com>
2010-11-15 14:10:14 +0200

    updating setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "smn-richmond",
-    version = "0.1",
-    url = 'http://github.com/smn/richmond',
+    name = "praekelt-vumi",
+    version = "0.01",
+    url = 'http://github.com/praekelt/vumi',
     license = 'BSD',
-    description = "Mobile messaging infrastructure.",
-    author = 'Simon de Haan',
+    description = "Super-scalable messaging engine for the delivery of SMS, " \
+                  "Star Menu and chat messages to diverse audiences in " \
+                  "emerging markets and beyond.",
+    author = 'Praekelt Foundation',
     packages = find_packages('src'),
     package_dir = {'': 'src'},
     install_requires = ['setuptools',],
